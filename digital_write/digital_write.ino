eef841cd3bb9600a6a631cf5372a9dc7ec84ed84
Ariel Churi <git@sparklelabs.com>
2015-09-22 17:34:12 -0400

    format fixes

--- digital_write/digital_write.ino
+++ digital_write/digital_write.ino
@@ -7,12 +7,13 @@ void setup() {
 void loop() {
   x = x + 1;
   if (x == 30000) {
-  digitalWrite(ledPin, HIGH);
+    digitalWrite(ledPin, HIGH);
   }
   if (x == 0) {
-  digitalWrite(ledPin, LOW);
-  x = 0;
+    digitalWrite(ledPin, LOW);
+    x = 0;
   }
-//The serial port slows your program down so comment it out.
-//Serial.println(x);
+  //The serial port slows your program down so comment it out.
+ Serial.print("fred equals ");
+  Serial.println(x);
 }
--- variables_for_pins/variables_for_pins.ino
+++ variables_for_pins/variables_for_pins.ino
@@ -1,11 +1,13 @@
+int x = 0;
 int ledPin = 13;
 void setup() {
   pinMode(ledPin, OUTPUT);
-   //Serial.begin(9600);
+  Serial.begin(9600);
 }
 void loop() {
   digitalWrite(ledPin, HIGH);
-  //Serial.println(ledPin);
+  x = x + 1;
+  Serial.println(x);
   delay(1000);
   digitalWrite(ledPin, LOW);
   delay(1000);
